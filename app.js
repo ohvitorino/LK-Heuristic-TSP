91795e4b1f840c9c80f664db37c44242bf66dfd9
Bruno Vitorino <bruno.vitorino@gmail.com>
2016-05-05 22:40:38 +0200

    New test points.

--- app.js
+++ app.js
@@ -4,34 +4,36 @@
 
 
 var points = [
-    {x: 50, y: 10, name: 'a'},
-    {x: 100, y: 30, name: 'b'},
-    {x: 50, y: 50, name: 'c'},
-    {x: 50, y: 60, name: 'd'},
-    {x: 60, y: 70, name: 'e'},
-    {x: 40, y: 80, name: 'f'},
-    {x: 50, y: 50, name: 'c'}
+    // {x: 50, y: 10, name: 'a'},
+    // {x: 100, y: 30, name: 'b'},
+    // {x: 50, y: 50, name: 'c'},
+    // {x: 50, y: 60, name: 'd'},
+    // {x: 60, y: 70, name: 'e'},
+    // {x: 40, y: 80, name: 'f'},
+    // {x: 50, y: 50, name: 'c'}
 
-    // {x: 60, y: 200},
-    // {x: 180, y: 200},
-    // {x: 80, y: 180},
-    // {x: 140, y: 180},
-    // {x: 20, y: 160},
-    // {x: 100, y: 160},
-    // {x: 200, y: 160},
-    // {x: 140, y: 140},
-    // {x: 40, y: 120},
-    // {x: 100, y: 120},
-    // {x: 180, y: 100},
-    // {x: 60, y: 80},
-    // {x: 120, y: 80},
-    // {x: 180, y: 60},
-    // {x: 20, y: 40},
-    // {x: 100, y: 40},
-    // {x: 200, y: 40},
-    // {x: 20, y: 20},
-    // {x: 60, y: 20},
-    // {x: 160, y: 20}
+    {x: 160, y: 20, name: 'a'},
+    {x: 60, y: 20, name: 'b'},
+    {x: 20, y: 20, name: 'c'},
+    {x: 200, y: 40, name: 'd'},
+    {x: 100, y: 40, name: 'e'},
+    {x: 20, y: 40, name: 'f'},
+    {x: 180, y: 60, name: 'g'},
+    {x: 120, y: 80, name: 'h'},
+    {x: 60, y: 80, name: 'i'},
+    {x: 180, y: 100, name: 'j'},
+    {x: 100, y: 120, name: 'k'},
+    {x: 40, y: 120, name: 'l'},
+    {x: 140, y: 140, name: 'm'},
+    {x: 200, y: 160, name: 'n'},
+    {x: 100, y: 160, name: 'o'},
+    {x: 20, y: 160, name: 'p'},
+    {x: 140, y: 180, name: 'q'},
+    {x: 80, y: 180, name: 'r'},
+    {x: 180, y: 200, name: 's'},
+    {x: 60, y: 200, name: 't'},
+    {x: 20, y: 160, name: 'u'}
+
 ];
 
 app.drawEdges(points);
@@ -42,3 +44,5 @@ var lk = new app.tsp.LinKernighan(points);
 var finalTour = lk.solve();
 
 output.println("Tour final: " + finalTour.toString());
+
+app.drawEdges(finalTour.tour);
